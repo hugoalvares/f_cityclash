873b42c2e61aa07efa3ae0b41c70e1c8d8856a26
Italo <italomarcosbd@gmail.com>
2015-05-31 17:21:05 -0300

    Segunda tentativa para atualizar as regras

--- server/regras/investimento.js
+++ server/regras/investimento.js
@@ -1,8 +1,13 @@
 var db = require('../database.js');
 
 module.exports = {
-	cadastraInvestimento : function(nome) {
-		db.rodaSql('insert into gestor (nome, email, senha) values ('+ nome +')', function(){
+	cadastraInvestimento : function(nome, preco, tempoconstrucao, manutencao) {
+		db.rodaSql('insert into gestor (nome, preco, tempoconstrucao, manutencao) values ('+ nome +','+ preco +','+ tempoconstrucao +','+ manutencao +')', function(){
+			callback();
+		});
+	},
+	cadastraReceita : function(tiporeceita_codigo, estrutura_codigo, valor) {
+		db.rodaSql('insert into gestor (tiporeceita_codigo, estrutura_codigo, valor) values ('+tiporeceita_codigo+','+estrutura_codigo+','+ valor +')', function(){
 			callback();
 		});
 	},

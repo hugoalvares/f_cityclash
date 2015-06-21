0a60c8fa5c1030ed8f1f32b3651923f10889d53e
Italo <italomarcosbd@gmail.com>
2015-06-21 17:45:26 -0300

    Criacao de script para insercao na tabela investimento

---
+++ documentacao/InserçãoInvestimento.sql
@@ -0,0 +1,53 @@
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Padaria', 5000.00, 013000, 500.00, 333.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Açougue', 6000.00, 023000, 600.00, 400.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Bomboniere', 4500.00, 010000, 450.00, 300.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Cabeleleiro', 2500.00, 003000, 250.00, 167.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Farmacia', 5500.00, 020000, 550.00, 367.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Sacolão', 4000.00, 004500, 400.00, 267.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Papelaria', 3000.00, 010000, 300.00, 200.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Loja de Brinquedo', 10000.00, 240000, 1000.00, 667.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Livraria', 9500.00, 100000, 950.00, 633.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Hotel', 20000.00, 480000, 2000.00, 1333.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Restaurante', 9000.00, 090000, 900.00, 600.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Fabrica de Calçados', 30000.00, 010000, 450.00, 300.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Fabrica de Massas', 45000.00, 850000, 4500.00, 3000.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Fabrica de Tecidos', 35000.00, 750000, 3500.00, 2333.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Fabrica de Automoveis', 50000.00, 990000, 5000.00, 3333.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Fabrica de Borracha', 20000.00, 480000, 2000.00, 1333.00)
+
+insert into investimento (nome, preco, tempoconstrucao, manutencao, rendimento)
+values ('Fabrica de Ferro', 27000.00, 550000, 2700.00, 1800.00)
+
+
+
